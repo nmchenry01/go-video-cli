aa3a35e19cacf2ac518b71ed947f61108a6afaab
nmchenry01 <nicholas.mchenry@levvel.io>
2019-07-19 17:15:21 -0700

    add persistent flag to root command

--- cmd/root.go
+++ cmd/root.go
@@ -5,12 +5,20 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+var author string
 
 var rootCmd = &cobra.Command{
 	Use:   "go-video-cli",
 	Short: "A simple cli for encrypting files, decrypting files, uploading files to S3, and deleting files from S3",
 	Long:  "A simple cli for encrypting files, decrypting files, uploading files to S3, and deleting files from S3",
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVar(&author, "author", "Nicholas McHenry", "Author name for copyright attribution")
+	viper.BindPFlag("author", rootCmd.PersistentFlags().Lookup("author"))
 }
 
 // Execute : The main function to execute the rootCmd

4f0fab238b8afec0a5d5bf65b18daddfa34ae9fc
nmchenry01 <nicholas.mchenry@levvel.io>
2019-07-20 10:55:48 -0700

    add helper method for decryption

--- cmd/decrypt.go
+++ cmd/decrypt.go
@@ -1,8 +1,12 @@
 package cmd
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"fmt"
 
+	"github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -18,4 +22,35 @@ var decrypt = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Hello World!")
 	},
+}
+
+func createPlaintext(data *[]byte, passphrase string) []byte {
+	key := []byte(createHash(passphrase))
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		log.WithFields(logrus.Fields{
+			"error": err.Error(),
+		}).Fatal("There was an issue creating the cipher block")
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		log.WithFields(logrus.Fields{
+			"error": err.Error(),
+		}).Fatal("There was an issue creating the GCM block")
+	}
+
+	encryptedData := *data
+	nonceSize := gcm.NonceSize()
+
+	nonce, ciphertext := encryptedData[:nonceSize], encryptedData[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		log.WithFields(logrus.Fields{
+			"error": err.Error(),
+		}).Fatal("There was an issue creating the decrypting the ciphertext")
+	}
+
+	return plaintext
 }
